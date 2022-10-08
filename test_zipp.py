25285159390d1ff78f62b0361f647ad0a9dd0ce4
Jason R. Coombs <jaraco@jaraco.com>
2022-10-08 11:20:14 -0400

    Test assertions directly

--- test_zipp.py
+++ test_zipp.py
@@ -419,27 +419,21 @@ class TestPath(unittest.TestCase):
             assert isinstance(file, cls)
 
     def test_can_pickle_string_path(self):     
-        try:
-            path_1 = zipp.Path("/path/to/a/file.zip")
-            path_1_pickle = pickle.dumps(path_1)
-            path_1_load = pickle.loads(path_1_pickle)
-            assert path_1.root == path_1_load.root
-            path_2 = zipp.Path("/path/to/a/file.zip", at="something.txt")
-            path_2_pickle = pickle.dumps(path_2)
-            path_2_load = pickle.loads(path_2_pickle)
-            assert path_2.root == path_2_load.root
-        except TypeError as exec:
-            assert False, "TypeError: Path is not pickleable"
+        path_1 = zipp.Path("/path/to/a/file.zip")
+        path_1_pickle = pickle.dumps(path_1)
+        path_1_load = pickle.loads(path_1_pickle)
+        assert path_1.root == path_1_load.root
+        path_2 = zipp.Path("/path/to/a/file.zip", at="something.txt")
+        path_2_pickle = pickle.dumps(path_2)
+        path_2_load = pickle.loads(path_2_pickle)
+        assert path_2.root == path_2_load.root
 
     def test_can_pickle_pathlib_path(self):
-        try:
-            path_1 = zipp.Path(pathlib.Path("/path/to/a/file.zip"))
-            path_1_pickle = pickle.dumps(path_1)
-            path_1_load = pickle.loads(path_1_pickle)
-            assert path_1.root == path_1_load.root
-            path_2 = zipp.Path(pathlib.Path("/path/to/a/file.zip", at="something.txt"))
-            path_2_pickle = pickle.dumps(path_2)
-            path_2_load = pickle.loads(path_2_pickle)
-            assert path_2.root == path_2_load.root
-        except TypeError as exec:
-            assert False, "TypeError: Path is not pickleable"
+        path_1 = zipp.Path(pathlib.Path("/path/to/a/file.zip"))
+        path_1_pickle = pickle.dumps(path_1)
+        path_1_load = pickle.loads(path_1_pickle)
+        assert path_1.root == path_1_load.root
+        path_2 = zipp.Path(pathlib.Path("/path/to/a/file.zip", at="something.txt"))
+        path_2_pickle = pickle.dumps(path_2)
+        path_2_load = pickle.loads(path_2_pickle)
+        assert path_2.root == path_2_load.root
