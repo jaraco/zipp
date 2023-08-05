793c3597f29308db813e38ca4a9b6312c435cfed
Jason R. Coombs <jaraco@jaraco.com>
2023-08-05 12:46:30 -0400

    Add support for arbitrary separators in glob.Translator, defaulting to os.sep + os.altsep.

--- zipp/glob.py
+++ zipp/glob.py
@@ -1,37 +1,58 @@
+import os
 import re
 
 
-def translate(pattern):
-    """
-    Given a glob pattern, produce a regex that matches it.
-    """
-    return extend(translate_core(pattern))
+_default_seps = os.sep + str(os.altsep) * bool(os.altsep)
 
 
-def extend(pattern):
-    r"""
-    Extend regex for pattern-wide concerns.
+class Translator:
+    seps: str
 
-    Apply '(?s:)' to create a non-matching group that
-    matches newlines (valid on Unix).
+    def __init__(self, seps: str = _default_seps):
+        assert seps in ('/', '\\', '\\/')
+        self.seps = _default_seps
 
-    Append '\Z' to imply fullmatch even when match is used.
-    """
-    return rf'(?s:{pattern})\Z'
+    def translate(self, pattern):
+        """
+        Given a glob pattern, produce a regex that matches it.
+        """
+        return self.extend(self.translate_core(pattern))
 
+    def extend(self, pattern):
+        r"""
+        Extend regex for pattern-wide concerns.
 
-def translate_core(pattern):
-    r"""
-    Given a glob pattern, produce a regex that matches it.
+        Apply '(?s:)' to create a non-matching group that
+        matches newlines (valid on Unix).
 
-    >>> translate_core('*.txt')
-    '[^/]*\\.txt'
-    >>> translate_core('a?txt')
-    'a[^/]txt'
-    >>> translate_core('**/*')
-    '.*/[^/]*'
-    """
-    return ''.join(map(replace, separate(pattern)))
+        Append '\Z' to imply fullmatch even when match is used.
+        """
+        return rf'(?s:{pattern})\Z'
+
+    def translate_core(self, pattern):
+        r"""
+        Given a glob pattern, produce a regex that matches it.
+
+        >>> t = Translator()
+        >>> t.translate_core('*.txt').replace('\\\\', '')
+        '[^/]*\\.txt'
+        >>> t.translate_core('a?txt')
+        'a[^/]txt'
+        >>> t.translate_core('**/*').replace('\\\\', '')
+        '.*/[^/]*'
+        """
+        return ''.join(map(self.replace, separate(pattern)))
+
+    def replace(self, match):
+        """
+        Perform the replacements for a match from :func:`separate`.
+        """
+        return match.group('set') or (
+            re.escape(match.group(0))
+            .replace('\\*\\*', r'.*')
+            .replace('\\*', rf'[^{re.escape(self.seps)}]*')
+            .replace('\\?', r'[^/]')
+        )
 
 
 def separate(pattern):
@@ -44,16 +65,3 @@ def separate(pattern):
     ['a', '[?]', 'txt']
     """
     return re.finditer(r'([^\[]+)|(?P<set>[\[].*?[\]])|([\[][^\]]*$)', pattern)
-
-
-def replace(match):
-    """
-    Perform the replacements for a match from :func:`separate`.
-    """
-
-    return match.group('set') or (
-        re.escape(match.group(0))
-        .replace('\\*\\*', r'.*')
-        .replace('\\*', r'[^/]*')
-        .replace('\\?', r'[^/]')
-    )
