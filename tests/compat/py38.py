e98868046dfb106bf4ece05f7fc7ad96f57f3606
Jason R. Coombs <jaraco@jaraco.com>
2024-08-21 21:09:51 -0400

    🧎‍♀️ Genuflect to the types.

--- tests/compat/py38.py
+++ tests/compat/py38.py
@@ -2,7 +2,7 @@ import sys
 
 
 if (3, 9) <= sys.version_info < (3, 11):  # pragma: no cover
-    from importlib.abc import Traversable
+    from importlib.abc import Traversable  # type: ignore[attr-defined, unused-ignore]
 elif sys.version_info < (3, 9):  # pragma: no cover
     from importlib_resources.abc import Traversable
 
--- zipp/compat/py310.py
+++ zipp/compat/py310.py
@@ -6,4 +6,8 @@ def _text_encoding(encoding, stacklevel=2, /):  # pragma: no cover
     return encoding
 
 
-text_encoding = io.text_encoding if sys.version_info > (3, 10) else _text_encoding
+text_encoding = (
+    io.text_encoding  # type: ignore[unused-ignore, attr-defined]
+    if sys.version_info > (3, 10)
+    else _text_encoding
+)
