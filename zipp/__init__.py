a706776b10b086d19a63b49a88989b3b69df5d33
Jason R. Coombs <jaraco@jaraco.com>
2025-05-26 09:49:00 -0400

    Tweak PyPy stacklevel adjustment.
    Closes #143

--- zipp/__init__.py
+++ zipp/__init__.py
@@ -198,7 +198,10 @@ class FastLookup(CompleteDirs):
 def _extract_text_encoding(encoding=None, *args, **kwargs):
     # compute stack level so that the caller of the caller sees any warning.
     is_pypy = sys.implementation.name == 'pypy'
-    stack_level = 3 + is_pypy
+    # PyPy no longer special cased after 7.3.19 (or maybe 7.3.18)
+    # See jaraco/zipp#143
+    is_old_pypi = is_pypy and sys.pypy_version_info < (7, 3, 19)
+    stack_level = 3 + is_old_pypi
     return text_encoding(encoding, stack_level), args, kwargs
 
 
